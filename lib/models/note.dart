ef25eae1686db0376ca2191a6097fea1dc39f233
André Pedersen <andrped94@gmail.com>
2024-08-05 18:33:41 +0200

    Update note.dart

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,33 +1,34 @@
 class Note {
-	int id;
-	String title;
-	String content;
-	String noteColor;
+  int? id;
+  String title;
+  String content;
+  String noteColor;
 
-	Note({
-		this.id = null, 
-		this.title = "Note", 
-		this.content = "Text", 
-		this.noteColor = 'red'
-	});
+  Note({
+    this.id, // `id` can be null
+    this.title = "Note",
+    this.content = "Text",
+    this.noteColor = 'red',
+  });
 
-	Map<String, dynamic> toMap() {
-		Map<String, dynamic> data = Map<String, dynamic>();
-		if (id != null) {
-			data['id'] = id;
-		}
-		data['title'] = title;
-		data['content'] = content;
-		data['noteColor'] = noteColor;
-		return data;
-	}
+  Map<String, dynamic> toMap() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (id != null) {
+      data['id'] = id;
+    }
+    data['title'] = title;
+    data['content'] = content;
+    data['noteColor'] = noteColor;
+    return data;
+  }
 
-	@override toString() {
-		return {
-			'id': id,
-			'title': title,
-			'content': content,
-			'noteColor': noteColor,
-		}.toString();
-	}
+  @override
+  String toString() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'noteColor': noteColor,
+    }.toString();
+  }
 }
